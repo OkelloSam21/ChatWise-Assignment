afd213eb96e30dc110c260e16307ef728b337c3a
OkelloSam <samuelokello929@gmail.com>
2024-08-07 07:23:58 +0300

    feat: Improve UI of main activity
    This commit improves the UI of the main activity.
    
    - Added margins to the "View Products" button.
    -
     Increased the size of the button.
    - Applied a custom style to the button for a consistent look.

--- app/src/main/java/com/samuelokello/chatwiseassignment/ui/MainActivity.kt
+++ app/src/main/java/com/samuelokello/chatwiseassignment/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.samuelokello.chatwiseassignment.ui
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
